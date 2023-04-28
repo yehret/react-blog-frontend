6674e7bec98ab101d61fc5a80db4829624b39dcd
Serhii <sirozhka.v@gmail.com>
2023-04-28 16:01:02 +0200

    add authorization, auth slice

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,28 +1,62 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 
 import styles from './Login.module.scss';
+import { fetchAuth } from '../../redux/slices/auth';
 
 export const Login = () => {
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
+  const onSubmit = (values) => {
+    dispatch(fetchAuth(values));
+  };
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Login
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Email is not correct"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Password" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          type="email"
+          label="E-Mail"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          {...register('email', { required: 'Email is required' })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          type="password"
+          label="Password"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register('password', { required: 'Enter your password' })}
+          fullWidth
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Sign in
+        </Button>
+      </form>
     </Paper>
   );
 };
