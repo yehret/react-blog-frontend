b5cd50c06363203333912a70d078de06f484b39f
Serhii <sirozhka.v@gmail.com>
2023-04-28 14:30:40 +0200

    rtk, fetch and dispatch posts

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Button from '@mui/material/Button';
 
 import styles from './Header.module.scss';
@@ -13,27 +15,27 @@ export const Header = () => {
     <div className={styles.root}>
       <Container maxWidth="lg">
         <div className={styles.inner}>
-          <a className={styles.logo} href="/">
+          <Link className={styles.logo} to="/">
             <div>MERN BLOG</div>
-          </a>
+          </Link>
           <div className={styles.buttons}>
             {isAuth ? (
               <>
-                <a href="/posts/create">
+                <Link to="/posts/create">
                   <Button variant="contained">Write article</Button>
-                </a>
+                </Link>
                 <Button onClick={onClickLogout} variant="contained" color="error">
                   Exit
                 </Button>
               </>
             ) : (
               <>
-                <a href="/login">
+                <Link to="/login">
                   <Button variant="outlined">Login</Button>
-                </a>
-                <a href="/register">
+                </Link>
+                <Link to="/register">
                   <Button variant="contained">Create account</Button>
-                </a>
+                </Link>
               </>
             )}
           </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -8,15 +9,19 @@ import './index.scss';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
 
+import store from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>,
+  </>,
 );
