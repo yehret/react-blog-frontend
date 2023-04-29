2c61f3ce442c87b2014c266bc9dbdd3384378d58
Serhii <sirozhka.v@gmail.com>
2023-04-29 14:18:59 +0200

    add user registration

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -41,8 +41,6 @@ export const Login = () => {
     }
   };
 
-  React.useEffect(() => {}, []);
-
   if (isAuth) {
     return <Navigate to="/" />;
   }
--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -6,8 +6,46 @@ import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 
 import styles from './Login.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { fetchRegister, selectIsAuth } from '../../redux/slices/auth';
+import { Navigate } from 'react-router-dom';
 
 export const Registration = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+  const {
+    register,
+    handleSubmit,
+    // setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      fullName: 'Test Name#',
+      email: 'test#@email.com',
+      password: '1234',
+    },
+    mode: 'onChange',
+  });
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchRegister(values));
+    if (!data.payload) {
+      return alert('Registration Failed');
+    }
+    if ('token' in data.payload) {
+      window.localStorage.setItem('token', data.payload.token);
+    } else {
+      alert('Registration failed');
+    }
+  };
+
+  React.useEffect(() => {}, []);
+
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
@@ -16,12 +54,38 @@ export const Registration = () => {
       <div className={styles.avatar}>
         <Avatar sx={{ width: 100, height: 100 }} />
       </div>
-      <TextField className={styles.field} label="Full name" fullWidth />
-      <TextField className={styles.field} label="E-Mail" fullWidth />
-      <TextField className={styles.field} label="Password" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Sign Up
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          type="text"
+          label="Full Name"
+          error={Boolean(errors.fullName?.message)}
+          helperText={errors.fullName?.message}
+          {...register('fullName', { required: 'Enter your name' })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          type="email"
+          label="E-Mail"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          {...register('email', { required: 'Email is required' })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          type="password"
+          label="Password"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register('password', { required: 'Enter your password' })}
+          fullWidth
+        />
+        <Button disabled={!isValid} type="submit" size="large" variant="contained" fullWidth>
+          Sign Up
+        </Button>
+      </form>
     </Paper>
   );
 };
